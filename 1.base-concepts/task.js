b23446f9cc523d3f8c7a3b55b58be56e022255fa
juliaIvankevich <juliashinobi@mail.ru>
2022-12-24 16:50:46 +0300

    changes task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
   
+  if (d === 0) {
+    arr.push((-b / (2 * a)));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false;
+  }
+
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let n = countMonths;
+  let paymentMonth = S * (P + (P / (((1 + P) ** n) - 1)));
+  return Number((paymentMonth * n).toFixed(2));
 }
