9df180a41fab3c44bcc221f50eac760efd960f4d
Julia Ivankevich <juliashinobi@mail.ru>
2023-01-04 12:53:03 +0300

    changes task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  let userFiltered = users.filter((user) => user.gender === gender);
+  let result = userFiltered.map((user) => user.age).reduce((a, b) => a + b, 0) / userFiltered.length; 
   
+  if (userFiltered.length === 0) {
+    result = 0;
+  }
+  
+  return result;
 }
